1f02120148a332cd29a7ef03ebbad51e98856dfb
Arquimedes Fermin Padua <arquimedesfermin17@gmail.com>
2020-01-29 13:21:39 -0400

    Subir Repositorio

---
+++ Example-Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumar sumar = new Sumar(4,4);
+
+            sumar.Calcular();
+
+
+            sumar.MostrarNumeros();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Example-Interfaces/Sumar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_Interfaces
+{
+   public class Sumar:ICalcul
+    {
+        public int a;
+        public int b;
+
+        public int total;
+
+        public Sumar(int primer,int segundo)
+        {
+            this.a = primer;
+            this.b = segundo;
+        }
+
+        public void MostrarNumeros() 
+        {
+
+
+            Console.WriteLine($"El primer numero a sumar es {a}");
+            Console.WriteLine($"El segundo numero a sumar es {b}");
+
+            Console.WriteLine($"El total de la suma es {total}");
+
+
+        }
+
+
+       public int Calcular() 
+        {
+            return total = a + b;
+        }
+
+
+
+
+    }
+}
